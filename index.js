1327405442bde54cf2bf8eb10f924be080f7d16b
Amrmamdouhezz <amrmamdouhamr1994@gmail.com>
2022-01-10 20:04:37 +0200

    adding new files

---
+++ index.js
@@ -0,0 +1,31 @@
+// image1
+var randomNumber1= Math.floor((Math.random()*6)+1);
+
+var randomDiceImage1="images/dice" + randomNumber1 + ".png";
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomDiceImage1);
+
+// image2
+
+var randomNumber2= Math.floor((Math.random()*6)+1);
+
+var randomDiceImage2="images/dice" + randomNumber2 + ".png";
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomDiceImage2);
+
+//changing h1 according to the winner
+
+if (randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").textContent= "the winner is number 1";
+}
+else if (randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").textContent= "the winner is number 2";
+}
+else { //both are equal
+    document.querySelector("h1").textContent= "No winner";
+}
